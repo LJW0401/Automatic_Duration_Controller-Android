1ae8e4b9cd08ca9e132356cfcb53341f72676708
是小企鹅呀 <1357482552@qq.com>
2023-07-28 22:22:07 +0800

    新建了一个空Android项目

---
+++ AndroidSRC/settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "自动刷时长器"
+include(":app")
